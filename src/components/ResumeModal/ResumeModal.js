90fb356b086c776c0fe517bd7520d728b39a6e36
Char Isabelo <charisseisabelo@gmail.com>
2021-11-06 05:08:10 -1000

    added and fixed framer motion animation

--- src/components/ResumeModal/ResumeModal.js
+++ src/components/ResumeModal/ResumeModal.js
@@ -3,6 +3,7 @@ import { useRef } from "react";
 import { AiOutlineDownload } from "react-icons/ai";
 import { RiCloseFill } from "react-icons/ri";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const ResumeModal = ({ showResumeModal, setShowResumeModal }) => {
   const resumeRef = useRef();
@@ -25,20 +26,22 @@ const ResumeModal = ({ showResumeModal, setShowResumeModal }) => {
           ref={resumeRef}
           onClick={closeResumeModal}
         >
-          <div className="resume-modal__img">
-            <Link
-              to="/files/CharIsabeloResume(SWE).pdf"
-              className="resume-modal__download-btn"
-              target="_blank"
-              download
-            >
-              <AiOutlineDownload className="resume-modal__icon" />
-              DOWNLOAD
-            </Link>
-            <button className="resume-modal__exit-btn" onClick={exitButton}>
-              <RiCloseFill className="resume-modal__exit-icon" />
-            </button>
-          </div>
+          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+            <div className="resume-modal__img">
+              <Link
+                to="/files/CharIsabeloResume(SWE).pdf"
+                className="resume-modal__download-btn"
+                target="_blank"
+                download
+              >
+                <AiOutlineDownload className="resume-modal__icon" />
+                DOWNLOAD
+              </Link>
+              <button className="resume-modal__exit-btn" onClick={exitButton}>
+                <RiCloseFill className="resume-modal__exit-icon" />
+              </button>
+            </div>
+          </motion.div>
         </div>
       ) : null}
     </>
