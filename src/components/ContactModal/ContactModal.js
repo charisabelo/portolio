b4b29f6d4f1a8039bac50877f6c5293f0fb0ed4a
Char Isabelo <charisseisabelo@gmail.com>
2021-11-06 04:37:17 -1000

    fixed bug

--- src/components/ContactModal/ContactModal.js
+++ src/components/ContactModal/ContactModal.js
@@ -2,6 +2,7 @@ import "./ContactModal.scss";
 import { GrFormClose } from "react-icons/gr";
 import { useRef } from "react";
 import { useSpring, animated } from "react-spring";
+// import { motion } from "framer-motion";
 import emailjs from "emailjs-com";
 import { EmojiProvider, Emoji } from "react-apple-emojis";
 import emojiData from "react-apple-emojis/lib/data.json";
@@ -10,13 +11,13 @@ const ContactModal = ({ showModal, setShowModal, openModal }) => {
   const modalRef = useRef();
   const form = useRef();
 
-  const animation = useSpring({
-    config: {
-      duration: 200,
-    },
-    opacity: showModal ? 1 : 0,
-    transform: showModal ? `translateY(0%)` : `translateY(-100rem)`,
-  });
+  // const animation = useSpring({
+  //   config: {
+  //     duration: 200,
+  //   },
+  //   opacity: showModal ? 1 : 0,
+  //   transform: showModal ? `translateY(0%)` : `translateY(-100rem)`,
+  // });
 
   const closeModal = (e) => {
     if (modalRef.current === e.target) {
@@ -50,103 +51,97 @@ const ContactModal = ({ showModal, setShowModal, openModal }) => {
     <>
       {showModal ? (
         <div className="contact-modal" ref={modalRef} onClick={closeModal}>
-          <animated.div style={animation}>
-            <div className="contact-modal__content">
-              <div className="contact-modal__icon-container">
-                <GrFormClose
-                  className="contact-modal__x-icon"
-                  onClick={openModal}
-                />
-              </div>
-              <div className="contact-modal__main-container">
-                <div className="contact-modal__header-container">
-                  <h1 className="contact-modal__header">contact me</h1>
-                  <EmojiProvider data={emojiData}>
-                    <Emoji
-                      name="love-letter"
-                      className="contact-modal__emoji"
-                    />
-                  </EmojiProvider>
-                </div>
-
-                <form
-                  ref={form}
-                  className="contact-modal__form"
-                  onSubmit={sendEmail}
-                >
-                  <div className="contact-modal__subject-container">
-                    <label htmlFor="" className="contact-modal__subject-label">
-                      Subject
-                    </label>
-                    <input
-                      type="text"
-                      className="contact-modal__subject"
-                      name="subject"
-                      placeholder="I would like to discuss a job"
-                      required
-                    />
-                  </div>
-                  <div className="contact-modal__name-container">
-                    <label htmlFor="" className="contact-modal__name-label">
-                      Name
-                    </label>
-                    <input
-                      type="text"
-                      placeholder="Patrick Star"
-                      name="name"
-                      className="contact-modal__firstname"
-                      required
-                    />
-                  </div>
-                  <div className="contact-modal__contact-container">
-                    <div className="contact-modal__email-container">
-                      <label htmlFor="" className="contact-modal__email-label">
-                        Email
-                      </label>
-                      <input
-                        type="email"
-                        className="contact-modal__email"
-                        name="email"
-                        placeholder="patrickstar@gmail.com"
-                        required
-                      />
-                    </div>
-                    <div className="contact-modal__phone-container">
-                      <label htmlFor="" className="contact-modal__phone-label">
-                        Phone Number(optional)
-                      </label>
-                      <input
-                        type="number"
-                        className="contact-modal__phone"
-                        name="phone"
-                        placeholder="optional"
-                      />
-                    </div>
-                    <div className="contact-modal__message-container">
-                      <label
-                        htmlFor=""
-                        className="contact-modal__message-label"
-                      >
-                        Message
-                      </label>
-                      <textarea
-                        type=""
-                        className="contact-modal__message"
-                        name="message"
-                        placeholder="Write your message here..."
-                        required
-                      ></textarea>
-                    </div>
-                  </div>
-                  <div className="contact-modal__btn-container">
-                    <button type="submit" className="contact-modal__btn">
-                      Send
-                    </button>
-                  </div>
-                </form>
-              </div>
+          {/* <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}> */}
+          <div className="contact-modal__content">
+            <div className="contact-modal__icon-container">
+              <GrFormClose
+                className="contact-modal__x-icon"
+                onClick={openModal}
+              />
             </div>
-          </animated.div>
+            <div className="contact-modal__main-container">
+              <div className="contact-modal__header-container">
+                <h1 className="contact-modal__header">contact me</h1>
+                <EmojiProvider data={emojiData}>
+                  <Emoji name="love-letter" className="contact-modal__emoji" />
+                </EmojiProvider>
+              </div>
+
+              <form
+                ref={form}
+                className="contact-modal__form"
+                onSubmit={sendEmail}
+              >
+                <div className="contact-modal__subject-container">
+                  <label htmlFor="" className="contact-modal__subject-label">
+                    Subject
+                  </label>
+                  <input
+                    type="text"
+                    className="contact-modal__subject"
+                    name="subject"
+                    placeholder="I would like to discuss a job"
+                    required
+                  />
+                </div>
+                <div className="contact-modal__name-container">
+                  <label htmlFor="" className="contact-modal__name-label">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Patrick Star"
+                    name="name"
+                    className="contact-modal__firstname"
+                    required
+                  />
+                </div>
+                <div className="contact-modal__contact-container">
+                  <div className="contact-modal__email-container">
+                    <label htmlFor="" className="contact-modal__email-label">
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      className="contact-modal__email"
+                      name="email"
+                      placeholder="patrickstar@gmail.com"
+                      required
+                    />
+                  </div>
+                  <div className="contact-modal__phone-container">
+                    <label htmlFor="" className="contact-modal__phone-label">
+                      Phone Number(optional)
+                    </label>
+                    <input
+                      type="number"
+                      className="contact-modal__phone"
+                      name="phone"
+                      placeholder="optional"
+                    />
+                  </div>
+                  <div className="contact-modal__message-container">
+                    <label htmlFor="" className="contact-modal__message-label">
+                      Message
+                    </label>
+                    <textarea
+                      type=""
+                      className="contact-modal__message"
+                      name="message"
+                      placeholder="Write your message here..."
+                      required
+                    ></textarea>
+                  </div>
+                </div>
+                <div className="contact-modal__btn-container">
+                  <button type="submit" className="contact-modal__btn">
+                    Send
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+          {/* </motion.div> */}
         </div>
       ) : null}
     </>
